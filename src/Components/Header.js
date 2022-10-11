9310c75239294efc961699269a80f530da754855
Rashmising11 <97082157+Rashmising11@users.noreply.github.com>
2022-10-11 14:30:44 +0530

    Add files via upload

---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Login</li>
+                <li>Sign Up</li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
